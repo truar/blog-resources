416caa696c860a5b7aad30cac5e919e0753b9d24
truar <thibault.ruaro@gmail.com>
2020-11-14 19:34:27 +0100

    first-commit

---
+++ gcp-skeleton/src/main/java/dev/truaro/com/gcpskeleton/GcpSkeletonApplication.java
@@ -0,0 +1,13 @@
+package dev.truaro.com.gcpskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSkeletonApplication.class, args);
+	}
+
+}
