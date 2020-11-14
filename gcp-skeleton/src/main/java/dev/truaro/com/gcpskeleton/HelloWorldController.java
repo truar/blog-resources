58179549f3b2f01030d2321d774184141dcf66e2
truar <thibault.ruaro@gmail.com>
2020-11-14 19:38:22 +0100

    Add hello world endpoint

---
+++ gcp-skeleton/src/main/java/dev/truaro/com/gcpskeleton/HelloWorldController.java
@@ -0,0 +1,14 @@
+package dev.truaro.com.gcpskeleton;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
