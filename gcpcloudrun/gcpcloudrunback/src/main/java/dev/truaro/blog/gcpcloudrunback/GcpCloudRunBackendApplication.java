548c654509e0daeed7b6ec8ddb2989896f57b32e
truar <thibault.ruaro@gmail.com>
2020-11-19 09:24:04 +0100

    Change back office to back end

--- gcpcloudrun/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/GcpCloudRunBackendApplication.java
+++ gcpcloudrun/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/GcpCloudRunBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GcpCloudRunBackOfficeApplication {
+public class GcpCloudRunBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GcpCloudRunBackOfficeApplication.class, args);
+		SpringApplication.run(GcpCloudRunBackendApplication.class, args);
 	}
 
 }
