601805c2557d79b438b5b5714f0c76329f21b1d7
truar <thibault.ruaro@gmail.com>
2021-04-01 15:10:35 +0200

    Add agent configuration

--- gcpapplication/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/GcpCloudRunBackendApplication.java
+++ gcpapplication/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/GcpCloudRunBackendApplication.java
@@ -2,12 +2,26 @@ package dev.truaro.blog.gcpcloudrunback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class GcpCloudRunBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcpCloudRunBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter
+				= new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA : [");
+		return filter;
 	}
 
 }
--- gcpapplication/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/HelloWorldController.java
+++ gcpapplication/gcpcloudrunback/src/main/java/dev/truaro/blog/gcpcloudrunback/HelloWorldController.java
@@ -1,13 +1,17 @@
 package dev.truaro.blog.gcpcloudrunback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping("/")
     public String helloWorld() {
+        logger.info("Hello World. I am deployed automatically");
         return "Hello World. I am deployed automatically";
     }
 
